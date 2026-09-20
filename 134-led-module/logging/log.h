719761f0b95867da4ca72ff4deb093d78c13cc20
zlydnev dmitry <zlydnev.dm@yandex.ru>
2026-09-21 00:14:40 +0300

    Сделана п1.3.7

--- 134-led-module/logging/log.h
+++ 134-led-module/logging/log.h
@@ -2,8 +2,10 @@
 
 #include <stdio.h>
 
-#define DEVICE_NAME "es-led-module"
-#define FIRMWARE_VERSION "1.0.0"
+//#include "device.h"
+
+//#define DEVICE_NAME "es-led-module"
+//#define FIRMWARE_VERSION "1.0.0"
 
 #define LOG_LEVEL_ERR 1
 #define LOG_LEVEL_INF 2
